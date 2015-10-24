0fbd6c837a0cc7c691ba3c639ceaf6760451e1d5
Lance Kindle <lance.kindle@gmail.com>
2015-10-24 12:10:49 -0700

    small cleanup. Rename some tests, add to some test descriptions

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ class TestList(unittest.TestCase):
         L = hookedup.List(hook={})
         L = hookedup.List()
 
-    def test_list_works_with_random_valid_hooks(self):
+    def test_random_valid_hooks(self):
         valid_hooks = self.post_hooks + self.pre_hooks
         count = 1
         while valid_hooks:
@@ -33,7 +33,7 @@ class TestList(unittest.TestCase):
             self.assertTrue(count == self.count)
             count += 1
 
-    def test_list_inits_with_supplied_items_from_iterable(self):
+    def test_list_inits_with_various_iterables(self):
         iterables = [(0,1,2), range(3), [0,1,2], {x: None for x in range(3)}]
         for iterable in iterables:
             ll = hookedup.List(iterable)
@@ -41,13 +41,16 @@ class TestList(unittest.TestCase):
             self.assertTrue(ll == [0,1,2])
 
     def trigger_all_hooks(self, L, n=1):
-        """ trigger (pre and post) add, replace, and remove events n-times each """
+        """ trigger (pre and post) add, replace, and remove events n-times each on list L"""
         for i in range(n):
             L.append(i)
             L[0] = i ** 2
             L.pop()
 
     def test_abort_prevents_post_events_from_triggering(self):
+        """ set all pre-hooks to auto-abort, and set all post-hooks to increment self.count. Verify
+        that post-hooks do not run until pre-hooks don't abort
+        """
         count = self.count
         premade_list = [0]  # compensate for hookup.List that prevents adding item
         for pre, post in zip(self.pre_hooks, self.post_hooks):
@@ -62,7 +65,7 @@ class TestList(unittest.TestCase):
             self.assertTrue(self.count == count + 1)
             count += 1
 
-    def test_replacing_with_slices(self):
+    def test_replacement_slices(self):
         """ test various slice replacement operations and verify that they match the expected
         behavior of a standard list """
         self.list = list(range(7))
@@ -71,7 +74,7 @@ class TestList(unittest.TestCase):
         self.assertTrue(self.list == L)
         self.assertTrue(len(self.list) == len(L))
         slices = [slice(1,3), slice(1,3,2), slice(4, 0, -1)]
-        replacements = [[-1,-2], [-3], [-4, -5, -6], [-7, -9, -9, -10, -11], []]
+        replacements = [[], [-1,-2], [-3], [-4, -5, -6], [-7, -9, -9, -10, -11]]
         successes = 0
         errors = 0
         error_types_expected = (ValueError,)
